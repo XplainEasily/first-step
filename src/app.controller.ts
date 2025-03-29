b1cbed769cb6fb2378d73fce08f796706e82fbc2
Lyes <lyes.coding@outlook.fr>
2025-03-29 17:16:25 +0100

    feat: create my first route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,15 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/fr")
+  getFr(): string {
+    return this.appService.getFr();
   }
 }
